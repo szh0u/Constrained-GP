f482e491810598feba50f5129cdfc431f62b5f84
Shuang Zhou <shuang.zhou4947@gmail.com>
2018-04-20 20:56:43 -0500

    Update c0GP.R

--- c0GP.R
+++ c0GP.R
@@ -1,17 +1,19 @@
 #########################################################
-# Input: x: Covariate, a n*1 vector. 
-#        y: Response, a n*1 vector.  
-#        nu: Smoothness parameter in matern kernel. 
-#        l: Length-scale parameter in matern kernel.
-#        niter: Iteration numvers of MC. 
-#        u: Knots of basis functions.
-#        Phi: Basis function evaluation, a n*(N+2) matrix.
-#        trans_mat: tansformation matrix used in the constrained GP methods
+# GP model incorporating constraints (1).
 #
-# Output: [f, tau, sigma] 
-#         f: mcmc samples of weights parameter  
-#         tau: mcmc samples of signal-to-noise level in matern kernel 
-#         sigma: mcmc samples of noise level
+# Input: x:         Covariate, a n*1 vector. 
+#        y:         Response, a n*1 vector.  
+#        nu:        Smoothness parameter in matern kernel. 
+#        l:         Length-scale parameter in matern kernel.
+#        niter:     Iteration numvers of MC. 
+#        u:         Knots of basis functions.
+#        Phi:       Basis function evaluation, a n*(N+2) matrix.
+#        trans_mat: tansformation matrix used in the constrained GP methods.
+#
+# Output: f:        mcmc samples of weights parameter.  
+#         tau:      mcmc samples of signal-to-noise level in matern kernel. 
+#         sigma:    mcmc samples of noise level.
+#
 ##########################################################
 
 
@@ -40,6 +42,8 @@ c0GP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   for(i in 2:niter){
     
     if(i%%10 == 0) print(i)
+    
+    # update f #
     Sig = tau[i-1]*t(Phi) %*% Phi + sigma[i-1]*mcov_inv
     Siginv = tau[i-1]*sigma[i-1]*chol2inv(chol(Sig))
     postmean = Siginv%*%t(Phi)%*%y/sigma[i-1]
@@ -54,7 +58,11 @@ c0GP = function(x, y, nu, l, niter, u, Phi, trans_mat){
     Sig_cond = (Siginv_22 - Siginv_21%*%Siginv_12/Siginv_11)
     f_cond = mu_cond + t(chol(Sig_cond))%*%rnorm(N+1,0, 1)
     f[,i] = as.matrix(c(1,f_cond), ncol = 1)
+    
+    # update sigma #
     sigma[i] = rinvgamma(1, shape = n/2, rate = t(y-Phi%*%f[,i])%*%(y-Phi%*%f[,i])/2)
+    
+    # update tau #
     tau[i] = rinvgamma(1, shape = (N+2)/2, rate = t(f[,i])%*%mcov_inv%*%f[,i]/2)
   }
   
@@ -63,3 +71,4 @@ c0GP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   return(re)
   
 }
+
