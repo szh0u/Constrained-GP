4b01e58ffcdf1b46da6a93ebbbe8d187a193109b
Shuang Zhou <shuang.zhou4947@gmail.com>
2018-04-20 20:57:46 -0500

    Update uGP.R

--- uGP.R
+++ uGP.R
@@ -1,19 +1,20 @@
 #########################################################
-# Input: x: Covariate, a n*1 vector. 
-#        y: Response, a n*1 vector.  
-#        nu: Smoothness parameter in matern kernel. 
-#        l: Length-scale parameter in matern kernel.
-#        niter: Iteration numvers of MC. 
-#        u: Knots of basis functions.
-#        Phi: Basis function evaluation, a n*(N+2) matrix.
-#        trans_mat: tansformation matrix used in the constrained GP methods
+# GP model with no constraints.
 #
-# Output: [f, tau, sigma] 
-#         f: mcmc samples of weights parameter  
-#         tau: mcmc samples of signal-to-noise level in matern kernel 
-#         sigma: mcmc samples of noise level
+# Input: x:         Covariate, a n*1 vector. 
+#        y:         Response, a n*1 vector.  
+#        nu:        Smoothness parameter in matern kernel. 
+#        l:         Length-scale parameter in matern kernel.
+#        niter:     Iteration numvers of MC. 
+#        u:         Knots of basis functions.
+#        Phi:       Basis function evaluation, a n*(N+2) matrix.
+#        trans_mat: tansformation matrix used in the constrained GP methods.
+#
+# Output: f:        mcmc samples of weights parameter.  
+#         tau:      mcmc samples of signal-to-noise level in matern kernel. 
+#         sigma:    mcmc samples of noise level.
+#
 ##########################################################
-
 
 uGP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   
@@ -41,11 +42,17 @@ uGP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   for(i in 2:niter){
     
     if(i%%10 == 0) print(i)
+    
+    # update f #
     Sig = tau[i-1]*t(Phi) %*% Phi + sigma[i-1]*mcov_inv
     Siginv = tau[i-1]*sigma[i-1]*chol2inv(chol(Sig))
     postmean = Siginv%*%t(Phi)%*%y/sigma[i-1]
     f[,i] = postmean + t(chol(Siginv))%*%rnorm(N+2, 0, 1)
+    
+    # update sigma #
     sigma[i] = rinvgamma(1, shape = n/2, rate = t(y-Phi%*%f[,i])%*%(y-Phi%*%f[,i])/2)
+    
+    # update tau #
     tau[i] = rinvgamma(1,shape = (N+2)/2, rate = t(f[,i])%*%mcov_inv%*%f[,i]/2)
   }
   
@@ -54,6 +61,4 @@ uGP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   return(re)
   
 }
-
-
 
