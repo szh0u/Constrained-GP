d2d167525d7bc206204f9f7038e37c6e0bc6bea4
Shuang Zhou <shuang.zhou4947@gmail.com>
2018-05-12 16:05:29 -0500

    Update cGP.R

--- cGP.R
+++ cGP.R
@@ -22,29 +22,28 @@ cGP = function(x, y, nu, l, niter, u, Phi, trans_mat){
 
   n=length(x)
   
-  # calculate covariance matrix #
-  mcov = maternCov(u, l, nu = nu)
+  mcov1 = maternCov(u, l, nu = nu)
+  mcov0 = mcov1[-1,]
+  mcov = mcov0[,-1]
   D = eigen(mcov)$values
   U = eigen(mcov)$vectors
   for(i in 1:length(D)){ if(D[i] < 0) D[i] = -D[i]}
   mcov = U%*%diag(D)%*%t(U)
   mcov_inv = chol2inv(chol(mcov))
- 
+  
   # set initial values of [f, tau, sigma] # 
   tau = c()
   tau[1] = 0.01
   sigma = c()
   sigma[1] = 0.01
-  f = matrix(nrow = N+2, ncol = niter)
-  f[,1] = t(chol(mcov))%*%rnorm(N+2, 0, 1)*sqrt(tau[1])
+  f = matrix(nrow = N+1, ncol = niter)
+  f[,1] = t(chol(mcov))%*%rnorm(N+1, 0, 1)*sqrt(tau[1])
   
-  # Set the boundaries used in generating truncated normal r.v. #
   L = rep(0, N+1) 
   U = rep(Inf, N+1)
- 
-  
-  # gibbs sampling # 
   re = list()
+  
+  # mcmc # 
   for(i in 2:niter){
     
     if (i%%10 == 0) print(i)
@@ -53,26 +52,17 @@ cGP = function(x, y, nu, l, niter, u, Phi, trans_mat){
     Sig = tau[i-1]*t(Phi) %*% Phi + sigma[i-1]*mcov_inv
     Siginv = tau[i-1]*sigma[i-1]*chol2inv(chol(Sig))
     postmean = Siginv%*%t(Phi)%*%y/sigma[i-1]
-    
-    mu1 = postmean[1]
-    mu2 = postmean[-1]
-    Siginv_11 = Siginv[1,1]
-    Siginv_21 = as.matrix(Siginv[-1,1])
-    Siginv_12 = t(Siginv_21) 
-    Siginv_22 = Siginv[-1,-1]
-    
-    mu_cond = mu2 + Siginv_21/Siginv_11*(1-mu1)
-    Sig_cond = (Siginv_22 - Siginv_21%*%Siginv_12/Siginv_11)
-    tran_Sig = trans_mat%*%Sig_cond%*%t(trans_mat) 
-    f_trans = trans_mat %*% mu_cond + mvrandn((L-trans_mat%*%mu_cond), U, tran_Sig, 1)
+
+    tran_Sig = trans_mat%*%Siginv%*%t(trans_mat) 
+    f_trans = trans_mat %*% postmean + mvrandn((L-trans_mat%*%postmean), U, tran_Sig, 1)
     f_cond = solve(trans_mat, f_trans)
-    f[,i] = as.matrix(c(1,f_cond), ncol = 1)
+    f[,i] = as.matrix(f_cond, ncol = 1)
     
     # update sigma #
     sigma[i] = rinvgamma(1, shape = n/2, rate = t(y-Phi%*%f[,i])%*%(y-Phi%*%f[,i])/2)
     
     # update tau #
-    tau[i] = rinvgamma(1,shape = (N+2)/2, rate = t(f[,i])%*%mcov_inv%*%f[,i]/2)
+    tau[i] = rinvgamma(1,shape = (N+1)/2, rate = t(f[,i])%*%mcov_inv%*%f[,i]/2)
   }
   
   re = list(f, tau, sigma)
