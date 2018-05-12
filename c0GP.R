41ad70a0a0ecd7c13e2afcb50ccf258a857acde6
Shuang Zhou <shuang.zhou4947@gmail.com>
2018-05-12 16:07:13 -0500

    Update c0GP.R

--- c0GP.R
+++ c0GP.R
@@ -19,10 +19,12 @@
 
 c0GP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   
-  n=length(x)
+   n=length(x)
   
   # covariance matrix #  
-  mcov = maternCov(u, l, nu = nu)
+  mcov1 = maternCov(u, l, nu = nu)
+  mcov0 = mcov1[-1,]
+  mcov = mcov0[,-1]
   D = eigen(mcov)$values
   U = eigen(mcov)$vectors
   for(i in 1:length(D)){ if(D[i] < 0) D[i] = -D[i]}
@@ -34,11 +36,11 @@ c0GP = function(x, y, nu, l, niter, u, Phi, trans_mat){
   sigma = c()
   sigma[1] = 0.01
   mcov_inv = chol2inv(chol(mcov))
-  f = matrix(nrow = N+2, ncol = niter)
-  f[,1] = t(chol(mcov))%*%rnorm(N+2, 0, 1)*sqrt(tau[1])
+  f = matrix(nrow = N+1, ncol = niter)
+  f[,1] = t(chol(mcov))%*%rnorm(N+1, 0, 1)*sqrt(tau[1])
   re = list()
   
-  #gibbs sampling #
+  # mcmc runs #
   for(i in 2:niter){
     
     if(i%%10 == 0) print(i)
@@ -47,23 +49,14 @@ c0GP = function(x, y, nu, l, niter, u, Phi, trans_mat){
     Sig = tau[i-1]*t(Phi) %*% Phi + sigma[i-1]*mcov_inv
     Siginv = tau[i-1]*sigma[i-1]*chol2inv(chol(Sig))
     postmean = Siginv%*%t(Phi)%*%y/sigma[i-1]
-    mu1 = postmean[1]
-    mu2 = postmean[-1]
-    Siginv_11 = Siginv[1,1]
-    Siginv_21 = as.matrix(Siginv[-1,1])
-    Siginv_12 = t(Siginv_21) 
-    Siginv_22 = Siginv[-1,-1]
-    
-    mu_cond = mu2 + Siginv_21/Siginv_11*(1-mu1)
-    Sig_cond = (Siginv_22 - Siginv_21%*%Siginv_12/Siginv_11)
-    f_cond = mu_cond + t(chol(Sig_cond))%*%rnorm(N+1,0, 1)
-    f[,i] = as.matrix(c(1,f_cond), ncol = 1)
+    f_cond = postmean + t(chol(Siginv))%*%rnorm(N+1,0, 1)
+    f[,i] = as.matrix(f_cond, ncol = 1)
     
     # update sigma #
     sigma[i] = rinvgamma(1, shape = n/2, rate = t(y-Phi%*%f[,i])%*%(y-Phi%*%f[,i])/2)
     
     # update tau #
-    tau[i] = rinvgamma(1, shape = (N+2)/2, rate = t(f[,i])%*%mcov_inv%*%f[,i]/2)
+    tau[i] = rinvgamma(1, shape = (N+1)/2, rate = t(f[,i])%*%mcov_inv%*%f[,i]/2)
   }
   
   re = list(f, tau, sigma)
